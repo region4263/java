0aa91dca9e1616e3de33d5b1de03f06af4ab49c6
Sega <region4263@mail.ru>
2022-10-12 23:37:27 +0400

    add dz_3

---
+++ lesson_3_homework/Intersection.java
@@ -0,0 +1,31 @@
+package lesson_3_homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Intersection {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersection(nums2, nums1);
+        }
+
+        Set<Integer> setNums1 = new HashSet<>();
+        for (int i : nums1) {
+            setNums1.add(i);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int j : nums2) {
+            if (setNums1.contains(j)) {
+                set.add(j);
+            }
+        }
+
+        int[] array = new int[set.size()];
+        int i = 0;
+        for (int num : set) {
+            array[i++] = num;
+        }
+        return array;
+    }
+}
